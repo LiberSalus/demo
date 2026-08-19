c71da692f47e4ea56f63db94f4eae834c164a4ed
m4ck-y <m4ck-y@users.noreply.github.com>
2026-08-19 23:40:30 +0000

    deploy: 9e94fddcbcdfbdd581fefad3c38ed4f750f9e4bf

---
+++ assets/RespondeCuidate-B_hBulnh.js
@@ -0,0 +1,1 @@
+import{j as t}from"./react-vendor-xHlx2AOR.js";import"./jszip.min-cNUI8R4m.js";import"./index-Wefa2X7a.js";import"./mui-vendor-KBXKy3ZI.js";import"./date-vendor-JJVV5h4O.js";const s=()=>t.jsx("div",{children:"Responde y cuidate"});export{s as default};
