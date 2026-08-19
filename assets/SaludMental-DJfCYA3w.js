a45df80df5580a2c76490461944cd52943dfbbc5
m4ck-y <m4ck-y@users.noreply.github.com>
2026-08-19 23:25:28 +0000

    deploy: af6e7deb50770b33963f86d59a7193de3333dbb4

--- assets/SaludMental-DJfCYA3w.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./react-vendor-xHlx2AOR.js";import{u as a,M as o}from"./useMetricasArea-BQTTjZnE.js";import{A as s}from"./metricas.config-WToOXx9-.js";import"./index-CSo_rGcQ.js";import"./mui-vendor-KBXKy3ZI.js";import"./date-vendor-JJVV5h4O.js";const n=()=>{const t=a(s.SALUD_MENTAL);return r.jsx(o,{tituloSeccion:"Salud Mental",metricas:t})};export{n as default};
